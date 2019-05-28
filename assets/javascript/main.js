0c3e0ebbabb99041144b82523e71a8088cea4a20
donxiya <31514669+donxiya@users.noreply.github.com>
2019-05-27 19:56:35 -0500

    search function, cors request solvec

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,59 +10,73 @@ $(document).ready(function () {
     };
     var searchGame = function (term) {
         //need to change url
-        var queryURL = "https://api-endpoint.igdb.com/games/?search=" + term;
+        var key = "52e79fca4d325c1ee085a289f1703202d6089c8e";
+        var queryURL = "https://www.giantbomb.com/api/search?api_key=" + key + "&format=json&query=" + term + "&resources=game";
+        console.log(queryURL);
+        console.log("test");
+
         $.ajax({
-            method: "GET",
-            url: queryURL,
-            dataType: 'jsonp',
-            cors: true,
-            contentType: 'application/json',
-            secure: true,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                "user-key": "3577d4e3a555ea221225af170191c8b8",
-                Accept: "application/json",
+            url: "http://api.giantbomb.com/search/",
+            dataType: "jsonp",
+            jsonp: "json_callback",
+            data: {
+                api_key: "52e79fca4d325c1ee085a289f1703202d6089c8e",
+                query: term,
+                format: "jsonp",
+                field_list: "name",
+                resources: "game",
             },
         }).then(function (response) {
-            if (response.data != null) {
-                for (var i = 0; i < 10; i++) {
-                    //this div containts everything
-                    var itemDiv = $("<div>");
-                    itemDiv.addClass("itemId");
-                    itemDiv.attr("id", response.data[i].title);
-                    //this div is image
-                    var imageBody = $("<img>");
-                    imageBody.addClass("imageBody");
-                    imageBody.attr("id", term + i);
-                    imageBody.attr("src", response.data[i].image);
-                    var title = $("<div>");
-                    title.addClass("title");
-                    title.text(response.data[i].title);
-                    var description = $("<div>");
-                    description.addClass("description");
-                    description.text();
-                    var rating = $("<div>");
-                    rating.addClass("rating");
-                    rating.text(response.data[i].rating);
-                    var price = $("<div>");
-                    price.addClass("price");
-                    price.text(response.data[i].price);
-                    var linkVideo = $("<div>");
-                    linkVideo.addClass("linkVideo");
-                    linkVideo.text("link");
-                    var linkAmz = $("<div>");
-                    linkAmz.addClass("linkVideo");
-                    linkAmz.text("link");
-                    //gifDiv.append(t);
-                    $("#resultList").append(gifDiv);
-                }
-            };
-        });
+
+            console.log("test success");
+            console.log(response);
+        }
+
+        )
+
+
+        // .then(function (response) {
+        //     if (response.data != null) {
+        //         for (var i = 0; i < 10; i++) {
+        //             //this div containts everything
+        //             var itemDiv = $("<div>");
+        //             itemDiv.addClass("itemId");
+        //             itemDiv.attr("id", response.data[i].title);
+        //             //this div is image
+        //             var imageBody = $("<img>");
+        //             imageBody.addClass("imageBody");
+        //             imageBody.attr("id", term + i);
+        //             imageBody.attr("src", response.data[i].image);
+        //             var title = $("<div>");
+        //             title.addClass("title");
+        //             title.text(response.data[i].title);
+        //             var description = $("<div>");
+        //             description.addClass("description");
+        //             description.text();
+        //             var rating = $("<div>");
+        //             rating.addClass("rating");
+        //             rating.text(response.data[i].rating);
+        //             var price = $("<div>");
+        //             price.addClass("price");
+        //             price.text(response.data[i].price);
+        //             var linkVideo = $("<div>");
+        //             linkVideo.addClass("linkVideo");
+        //             linkVideo.text("link");
+        //             var linkAmz = $("<div>");
+        //             linkAmz.addClass("linkVideo");
+        //             linkAmz.text("link");
+        //             //gifDiv.append(t);
+        //             $("#resultList").append(gifDiv);
+        //         }
+        //     };
+        // });
     }
     $("#game-query").on("click", function () {
         if ($("#game-search").val() != "") {
+            var a = $("#game-search").val();
+            searchGame(a);
             newSearch();
-            searchGame(search);
+
         };
     });
 });
