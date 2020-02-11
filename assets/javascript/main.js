4c438462f6da115986edf09695e07df15c58fb9b
Ian Gacek <gacek004@umn.edu>
2020-02-11 12:25:29 -0600

    Commented out code for Release Date to allow game search to work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -97,13 +97,13 @@ $(document).ready(function () {
                         var info1 = $("<th>");
                         info1.attr("scope", "row");
                         info1.text("Release Date");
-                        var info2 = $("<th>");
-                        info2.attr("scope", "row");
-                        var date = response.results[i].original_release_date;
-                        date = date.split(' ')[0];
-                        info2.text(date);
-                        tr2.append(info1);
-                        tr2.append(info2);
+                        // var info2 = $("<th>");
+                        // info2.attr("scope", "row");
+                        // var date = response.results[i].original_release_date;
+                        // // date = date.split(' ')[0];
+                        // info2.text(date);
+                        // tr2.append(info1);
+                        // tr2.append(info2);
                         var tr3 = $("<tr>");
                         var info1 = $("<th>");
                         info1.attr("scope", "row");
@@ -115,7 +115,7 @@ $(document).ready(function () {
                         info2.attr("scope", "row");
                         var image = $("<img>")
                         image.attr("src", response.results[i].image.original_url);
-                        image.css("max-width", "100%");
+                        image.css("max-width", 300);
                         info2.append(image);
                         tr3.append(info1);
                         tr3.append(info2);
